41f5596d1937da2fee3805a34116d5348afbf35c
Vishal Chaudhary <vishalchaudhary5794@gmail.com>
2024-01-14 22:24:25 +0530

    finalizing #6

--- TazaKhabre/settings.py
+++ TazaKhabre/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env('ALLOWED_HOST').split(' ')
+ALLOWED_HOSTS = ["*"]
 
 API_KEY = env('API_KEY')
 
